007f9c548bb6c7902cc83a64b7742745df41345e
Mariana <63500992+Mariana-VV@users.noreply.github.com>
2023-09-18 14:50:22 +0200

    Properties

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,49 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
+    public Book() {
+    }
+
+    public Book(String title, String author,
+                String isbn, BigDecimal price,
+                String description, String coverImage) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.description = description;
+        this.coverImage = coverImage;
+    }
+
+    public Book(long id, String title,
+                String author, String isbn,
+                BigDecimal price, String description,
+                String coverImage) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.description = description;
+        this.coverImage = coverImage;
+    }
+}
