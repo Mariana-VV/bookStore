6b89e58241550b65cee225ff0b2480e1f58fb5bd
Mariana <63500992+Mariana-VV@users.noreply.github.com>
2023-09-23 15:44:14 +0200

    JPI

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,8 @@
 package com.example.bookstore;
 
-import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.service.BookService;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -15,7 +10,5 @@ public class BookStoreApplication {
     private BookService bookService;
 
     public static void main(String[] args) {
-
-        SpringApplication.run(BookStoreApplication.class, args);
     }
 }
