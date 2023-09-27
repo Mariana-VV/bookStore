87a7b2052006fb46d60ae844369081dfbb865fc9
Mariana <63500992+Mariana-VV@users.noreply.github.com>
2023-09-27 11:08:05 +0200

    Lines

--- src/main/java/com/example/bookstore/dto/BookDto.java
+++ src/main/java/com/example/bookstore/dto/BookDto.java
@@ -6,11 +6,8 @@ import lombok.Data;
 @Data
 public class BookDto {
     private Long id;
-
     private String title;
-
     private String author;
-
     private String isbn;
     private BigDecimal price;
     private String description;
